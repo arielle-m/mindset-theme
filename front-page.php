eb7f8e512fe7e89c520541f7d5bd2f2df193946e
arielle-m <ariem.marii@gmail.com>
2024-01-24 09:05:01 -0800

    corrected day 4 activities

--- front-page.php
+++ front-page.php
@@ -68,7 +68,7 @@ get_header();
 								<a href="<?php the_permalink(); ?>">
 								<?php the_post_thumbnail( 'landscape-blog' ); ?>
 									<h3><?php the_title() ?></h3>
-									<p><?php the_date() ?></p>
+									<p><?php echo get_the_date() ?></p>
 								</a>
 							</article>
 
