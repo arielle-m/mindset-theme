fd65bc5c67d4bb279bbed8b1d81db34a858986ee
arielle-m <ariem.marii@gmail.com>
2024-01-30 09:29:43 -0800

    day7 bonus task 2 attempt

--- archive-fwd-work.php
+++ archive-fwd-work.php
@@ -13,8 +13,9 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<header class="page-header">
+		<h1><?php single_term_title(); ?></h1>
 			<?php
-			the_archive_title( '<h1 class="page-title">', '</h1>' );
+			// the_archive_title( '<h1 class="page-title">', '</h1>' );
 			the_archive_description( '<div class="archive-description">', '</div>' );
 			?>
 		</header><!-- .page-header -->
